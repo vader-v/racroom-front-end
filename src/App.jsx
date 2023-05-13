77092c85b7361c7b224877b663065a49b797ac2d
Jerry Pao <kyjerry0424@gmail.com>
2023-05-13 15:11:52 -0400

    adjust trivia route

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
           }
         />
         <Route 
-          path='/triviaList'
+          path='/trivia'
           element={
             <ProtectedRoute user={user}>
               <TriviaList trivias={trivias} />
