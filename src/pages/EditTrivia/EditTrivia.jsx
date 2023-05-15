a828b340ff042be801b45e719a4e580a45feb2b1
Hunter Eckhoff <Phantazm777@verizon.net>
2023-05-15 14:25:27 -0400

    iterate using map to show the placeholders for each question in the edit page, and to only show as many answers someone has put, in the edit function.

--- src/pages/EditTrivia/EditTrivia.jsx
+++ src/pages/EditTrivia/EditTrivia.jsx
@@ -16,7 +16,7 @@ const EditTrivia = (props) => {
 
   const handleCheckbox = (e) => {
     setFormData({ ...formData, [e.target.name]: !!e.target.checked})
-  };
+  }
 
   const handleSubmit = (evt) => {
     evt.preventDefault()
@@ -26,7 +26,9 @@ const EditTrivia = (props) => {
   return (
     <main>
       <form onSubmit={handleSubmit}>
-				<label htmlFor="title-input">Title</label>
+				<label htmlFor="title-input">
+          Title
+        </label>
 				<input
           type="text" 
           name="title"
@@ -35,7 +37,9 @@ const EditTrivia = (props) => {
           placeholder="Title"
           onChange={handleChange}
         />
-        <label htmlFor="category-input">Category</label>
+        <label htmlFor="category-input">
+          Category
+        </label>
         <select 
           name="category" 
           id="category-input" 
@@ -44,13 +48,15 @@ const EditTrivia = (props) => {
         >
             <option value="Keyboard Shortcuts">Keyboard Shortcuts</option>
             <option value="Programming">Programming</option>
-            <option value="Pop-culture">Pop Culture</option>
+            <option value="Pop-Culture">Pop Culture</option>
             <option value="History">History</option>
             <option value="Games">Games</option>
             <option value="Languages">Languages</option>
             <option value="Television">Television</option>
         </select>
-        <label htmlFor="question-input">Question</label>
+        <label htmlFor="question-input">
+          Question
+        </label>
         <textarea
           required
           type="text"
@@ -61,61 +67,29 @@ const EditTrivia = (props) => {
           onChange={handleChange}
         >
         </textarea>
-        {formData.questions[0].choices[0] && (
-          <div>
-            <label htmlFor="answer1-input">Answer 1</label>
-            <input 
-              required
-              type="text"
-              name="answer1"
-              value={formData.answer1}
-              placeholder={formData.questions[0].choices[0].text}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        {formData.questions[0].choices[1] && (
-          <div>
-            <label htmlFor="answer2-input">Answer 2</label>
-            <input 
-              required
-              type="text"
-              name="answer2"
-              value={formData.answer2}
-              placeholder={formData.questions[0].choices[1].text}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        {formData.questions[0].choices[2] && (
-          <div>
-            <label htmlFor="answer3-input">Answer 3</label>
-            <input 
-              required
-              type="text"
-              name="answer3"
-              value={formData.answer3}
-              placeholder={formData.questions[0].choices[2].text}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        {formData.questions[0].choices[3] && (
-          <div>
-            <label htmlFor="answer4-input">Answer 4</label>
-            <input 
-              required
-              type="text"
-              name="answer4"
-              value={formData.answer4}
-              placeholder={formData.questions[0].choices[3].text}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-        <button type="submit">submit</button>
+        {formData.questions[0].choices.map((choice, index) => (
+          formData.questions[0].choices[index] && (
+            <div key={index}>
+              <label 
+                htmlFor={`answer${index + 1}-input`}>Answer {index + 1}
+              </label>
+              <input 
+                required
+                type="text"
+                name={`answer${index + 1}`}
+                value={formData[`answer${index + 1}`]}
+                placeholder={choice.text}
+                onChange={handleChange}
+              />
+            </div>
+          )
+        ))}
+        <button type="submit">
+          Submit
+        </button>
       </form>
     </main>
   )
 }
-  export default EditTrivia
+
+export default EditTrivia
