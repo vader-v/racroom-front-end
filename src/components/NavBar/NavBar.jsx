b40b55488b61eed412778b33465d68765fc77b37
andrewwinke123 <andrewwinke@gmail.com>
2023-05-16 10:51:01 -0400

    making the drop down more visible but also less in the way and giving it a button to close itself

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,56 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './NavBar.css'
 
 const NavBar = ({ user, handleLogout }) => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false)
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  }
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
 
-  return (
-    <nav>
-      <div onClick={toggleMenu} className="hamburger-icon">
-        &#9776; {/* This is a simple hamburger icon */}
-      </div>
-      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-        {user ? (
-          <>
-            <li>Welcome, {user.name}</li>
-            <li><NavLink to="/profiles">Profiles</NavLink></li>
-            <li><NavLink to="/trivia">All Trivia</NavLink></li>
-            <li><NavLink to="/trivia/new">Create</NavLink></li>
-            <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          </>
-        ) : (
-          <>
-            <li><NavLink to="/auth/login">Log In</NavLink></li>
-            <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-          </>
-        )}
-      </ul>
-    </nav>
-  )
+	const closeMenu = () => {
+		setIsOpen(false)
+	}
+
+	return (
+		<nav>
+			<div onClick={toggleMenu} className="hamburger-icon">
+				&#9776;
+			</div>
+			<ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+      <div onClick={closeMenu} className={`hamburger-icon ${isOpen ? 'inside' : ''}`}>
+          &#9776;
+        </div>
+				{user ? (
+					<>
+						<li>Welcome, {user.name}</li>
+						<li>
+							<NavLink to="/trivia" onClick={closeMenu}>Trivia</NavLink>
+						</li>
+						<li>
+							<NavLink to="/trivia/new" onClick={closeMenu}>Create</NavLink>
+						</li>
+						<li>
+							<NavLink to="/profiles" onClick={closeMenu}>Profiles</NavLink>
+						</li>
+						<li>
+							<NavLink to="" onClick={(event) => {handleLogout(event); closeMenu();}}>LOG OUT</NavLink>
+						</li>
+					</>
+				) : (
+					<>
+						<li>
+							<NavLink to="/auth/login" onClick={closeMenu}>Log In</NavLink>
+						</li>
+						<li>
+							<NavLink to="/auth/signup" onClick={closeMenu}>Sign Up</NavLink>
+						</li>
+					</>
+				)}
+			</ul>
+		</nav>
+	)
 }
 
 export default NavBar
