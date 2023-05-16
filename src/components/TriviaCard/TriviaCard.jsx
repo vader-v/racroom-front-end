249c87affd4cd5ebe97fb9cd16b2893403c3c672
Hunter Eckhoff <Phantazm777@verizon.net>
2023-05-16 14:19:12 -0400

    fix entire trivia card not being clickable and remove underlines

--- src/components/TriviaCard/TriviaCard.jsx
+++ src/components/TriviaCard/TriviaCard.jsx
@@ -10,18 +10,18 @@ import styles from './TriviaCard.module.css'
 const TriviaCard = ({ trivia }) => {
   return (
     <>
-      <article className={styles.container}>
-        <header>
-          <Link to={`/trivia/${trivia._id}`} state={trivia}>
-          <span>
-            <h1>{trivia.title}</h1>
-          </span>
-          <OwnerInfo content={trivia}/>
-          </Link>
-          <h6>Category:</h6>
-        <h4>{trivia.category}</h4>
-        </header>
-      </article>
+      <Link to={`/trivia/${trivia._id}`} state={trivia}>
+        <article className={styles.container}>
+          <header>
+            <span>
+              <h1>{trivia.title}</h1>
+            </span>
+            <OwnerInfo content={trivia}/>
+            <h6>Category:</h6>
+            <h4>{trivia.category}</h4>
+          </header>
+        </article>
+      </Link>
     </>
   )
 }
