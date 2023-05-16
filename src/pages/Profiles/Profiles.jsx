067d9e5412de2e39a71cbc11f9a8bd12c07ad1d0
Hunter Eckhoff <Phantazm777@verizon.net>
2023-05-16 13:35:48 -0400

    add search functionality and css and links to individual profiles in the profiles page

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,5 +1,6 @@
 // npm modules
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 // services
 import * as profileService from '../../services/profileService'
@@ -37,14 +38,18 @@ const Profiles = () => {
 
   return (
     <main className={styles.container}>
-      <h1>Hello. This is a list of all the profiles.</h1>
-      <SearchForm handleProfileSearch={handleProfileSearch} />
-      {<h2>{searchResults.length} results found</h2>}
-      {searchResults.map(profile =>
-        <div key={profile._id}>
-          {profile.name}
-        </div>
-      )}
+      <div className='stationary-search-bar'>
+        <h1>Hello. This is a list of all the profiles.</h1>
+        <SearchForm handleProfileSearch={handleProfileSearch} />
+        {<h2>{searchResults.length} results found</h2>}
+      </div>
+      <div className='profiles-name-search-list'>
+        {searchResults.map(profile =>
+          <Link to={`/profiles/${profile._id}`} key={profile._id} className='profile-names'>
+            {profile.name}
+          </Link>
+        )}
+      </div>
       <div className={styles.changePassword}>
         <NavLink to="/auth/change-password">Change Password</NavLink>
       </div>
