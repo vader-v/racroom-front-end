a7ef71e8f2a17ab2a151090f61052139fde20679
amirb <amir.bullock9@gmail.com>
2023-05-15 20:37:14 -0400

    create CategoryDropdown component

---
+++ src/components/CategoryDropdown/CategoryDropdown.jsx
@@ -0,0 +1,19 @@
+const CategoryDropdown = ({ value, onChange }) => {
+  return (
+    <select
+      name="category"
+      value={value}
+      onChange={onChange}
+    >
+      <option value="Keyboard Shortcuts">Keyboard Shortcuts</option>
+      <option value="Programming">Programming</option>
+      <option value="Pop Culture">Pop Culture</option>
+      <option value="History">History</option>
+      <option value="Games">Games</option>
+      <option value="Languages">Languages</option>
+      <option value="Television">Television</option>
+    </select>
+  )
+}
+
+export default CategoryDropdown
