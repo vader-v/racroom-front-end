4f51370095222a01d020bcac0f946ae865a69170
andrewwinke123 <andrewwinke@gmail.com>
2023-05-16 09:54:12 -0400

    make hamburger dropdown menu functional

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,35 @@
-// npm modules
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-
-// css
-
 import './NavBar.css'
 
 const NavBar = ({ user, handleLogout }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="/trivia">All Trivia</NavLink></li>
-          <li><NavLink to="/trivia/new">Create</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-        </ul>
-      :
-      <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
-      }
+      <div onClick={toggleMenu} className="hamburger-icon">
+        &#9776; {/* This is a simple hamburger icon */}
+      </div>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        {user ? (
+          <>
+            <li>Welcome, {user.name}</li>
+            <li><NavLink to="/profiles">Profiles</NavLink></li>
+            <li><NavLink to="/trivia">All Trivia</NavLink></li>
+            <li><NavLink to="/trivia/new">Create</NavLink></li>
+            <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
+          </>
+        ) : (
+          <>
+            <li><NavLink to="/auth/login">Log In</NavLink></li>
+            <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
+          </>
+        )}
+      </ul>
     </nav>
   )
 }
