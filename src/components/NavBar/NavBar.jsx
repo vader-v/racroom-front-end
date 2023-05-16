d3f0818519ddbb24bbf3b92e0401f85d7b24663f
andrewwinke123 <andrewwinke@gmail.com>
2023-05-16 09:37:18 -0400

    clean up nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,6 @@ const NavBar = ({ user, handleLogout }) => {
           <li><NavLink to="/profiles">Profiles</NavLink></li>
           <li><NavLink to="/trivia">All Trivia</NavLink></li>
           <li><NavLink to="/trivia/new">Create</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
         </ul>
       :
