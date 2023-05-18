b7936d4d3db46317788f171741acc9274ab82d89
amirb <amir.bullock9@gmail.com>
2023-05-18 15:43:16 -0400

    add basic styling to dropdown

--- src/components/CategoryDropdown/CategoryDropdown.jsx
+++ src/components/CategoryDropdown/CategoryDropdown.jsx
@@ -1,20 +1,21 @@
+// css
+import styles from "./CategoryDropdown.module.css";
+
 const CategoryDropdown = ({ value, onChange }) => {
   return (
-    <select
-      name="category"
-      value={value}
-      onChange={onChange}
-    >
-      <option value="">All</option>
-      <option value="Keyboard Shortcuts">Keyboard Shortcuts</option>
-      <option value="Programming">Programming</option>
-      <option value="Pop Culture">Pop Culture</option>
-      <option value="History">History</option>
-      <option value="Games">Games</option>
-      <option value="Languages">Languages</option>
-      <option value="Television">Television</option>
-    </select>
+    <div className={styles.dropdown}>
+      <select name="category" value={value} onChange={onChange}>
+        <option value="">All</option>
+        <option value="Keyboard Shortcuts">Keyboard Shortcuts</option>
+        <option value="Programming">Programming</option>
+        <option value="Pop Culture">Pop Culture</option>
+        <option value="History">History</option>
+        <option value="Games">Games</option>
+        <option value="Languages">Languages</option>
+        <option value="Television">Television</option>
+      </select>
+    </div>
   )
 }
 
-export default CategoryDropdown
+export default CategoryDropdown;
