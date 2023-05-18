bf980e3b4be467b4a57d80755461bb6ac66cba3a
amirb <amir.bullock9@gmail.com>
2023-05-18 12:13:32 -0400

    complete map functionality for trivias for profile view

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,24 +1,40 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import * as profileService from '../../services/profileService';
-
+import * as triviaService from '../../services/triviaService'
 const Profiles = () => {
   const { profileId } = useParams();
   const [profile, setProfile] = useState(null);
+  const [triviaDetails, setTriviaDetails] = useState([]);
 
+  
+  
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         const profileData = await profileService.getProfileById(profileId);
         setProfile(profileData);
+        if (profileData && profileData.trivia) {
+          // Fetch the details of each trivia based on their IDs
+          const fetchTriviaDetails = async () => {
+            const promises = profileData.trivia.map(async (triviaId) => {
+              const triviaData = await triviaService.getTriviaById(triviaId);
+              return triviaData;
+            });
+            const triviaDetailsData = await Promise.all(promises);
+            setTriviaDetails(triviaDetailsData);
+          };
+          
+          fetchTriviaDetails();
+        }
       } catch (error) {
         console.log(error);
       }
     };
-
+    
     fetchProfile();
   }, [profileId]);
-
+  
   if (!profile) {
     return <div>Loading...</div>;
   }
@@ -29,18 +45,28 @@ const Profiles = () => {
       <h3>Name: {profile.name}</h3>
       <img src={profile.photo} alt="Profile" />
       {/* Display the trivias associated with the profile */}
-      {profile.trivia && profile.trivia.length > 0 ? (
-        <div>
-          <h3>Trivias:</h3>
-          <ul>
-            {profile.trivia.map((triviaId) => (
-              <li key={triviaId}>{triviaId}</li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div>No trivias found.</div>
-      )}
+      {triviaDetails.length > 0 ? (
+  <div>
+    <h3>Trivias:</h3>
+    <ul>
+      {triviaDetails.map((trivia) => {
+        console.log(trivia); // Check the trivia object in the console
+        if (!trivia._id) {
+          console.log('Invalid trivia data:', trivia);
+          return null;
+        }
+        return (
+          <li key={trivia._id}>
+            <Link to={`/trivia/${trivia._id}`}>{trivia.title}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+) : (
+  <div>No trivias found.</div>
+)}
+
     </div>
   );
 };
