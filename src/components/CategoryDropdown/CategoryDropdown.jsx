7e2c7c062d1bd6ea16f4a7a4f423b2dbe97d8565
amirb <amir.bullock9@gmail.com>
2023-05-18 22:16:42 -0400

    update category options to match model update

--- src/components/CategoryDropdown/CategoryDropdown.jsx
+++ src/components/CategoryDropdown/CategoryDropdown.jsx
@@ -1,21 +1,34 @@
 // css
-import styles from "./CategoryDropdown.module.css";
+import styles from "./CategoryDropdown.module.css"
 
 const CategoryDropdown = ({ value, onChange }) => {
   return (
     <div className={styles.dropdown}>
       <select name="category" value={value} onChange={onChange}>
         <option value="">All</option>
-        <option value="Keyboard Shortcuts">Keyboard Shortcuts</option>
-        <option value="Programming">Programming</option>
         <option value="Pop Culture">Pop Culture</option>
         <option value="History">History</option>
         <option value="Games">Games</option>
         <option value="Languages">Languages</option>
         <option value="Television">Television</option>
+        <option value="Technology">Technology</option>
+        <option value="General Knowledge">General Knowledge</option>
+        <option value="Sports">Sports</option>
+        <option value="Music">Music</option>
+        <option value="Science">Science</option>
+        <option value="Fashion">Fashion</option>
+        <option value="Politics">Politics</option>
+        <option value="Nature">Nature</option>
+        <option value="Business">Business</option>
+        <option value="Fiction">Fiction</option>
+        <option value="Geography">Geography</option>
+        <option value="Food & Drink">Food/Drink</option>
+        <option value="Brain Teasers">Brain Teasers</option>
+        <option value="Mythology & Folklore">Mythology/Folklore</option>
+        <option value="Art & Literature">Art/Literature</option>
       </select>
     </div>
   )
 }
 
-export default CategoryDropdown;
+export default CategoryDropdown
